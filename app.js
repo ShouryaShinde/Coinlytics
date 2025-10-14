df66682b488da8067a74622ad3d9f9caa9600e4e
Shourya Shinde <shindeshauryaanil@gmail.com>
2025-10-14 23:14:54 +0530

    Added Header

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express" ;
+import bodyparser from "body-parser" ;
+import path from "path";
+import fs from "fs";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/" , (req,res)=> {
+  res.render("index.ejs") ;
+})
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
