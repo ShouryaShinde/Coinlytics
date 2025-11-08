c0afb8deb5e29f131d5bab5c22b32501f74727e2
Shourya Shinde <shindeshauryaanil@gmail.com>
2025-11-08 16:56:50 +0530

    Added Total Market Cap card in dashboard section

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ import express from "express" ;
 import bodyparser from "body-parser" ;
 import path from "path";
 import fs from "fs";
+import axios from "axios" ;
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/" , (req,res)=> {
-  res.render("index.ejs") ;
-})
+app.get("/" , async (req,res)=> {
+  try {
+    const response = await axios.get("https://api.coingecko.com/api/v3/global") ;
+    const result = response.data ;
+    res.render("index.ejs" , {data : result.data}) ;
+  }  catch(error) {
+    console.log("Error")
+    res.render("index.ejs" , {data : null})
+  }
+});
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
