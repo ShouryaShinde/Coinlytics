e638d5b930f1d42d4651a830091bf277463718c7
Shourya Shinde <shindeshauryaanil@gmail.com>
2025-11-08 22:48:50 +0530

    Market Overview Section Completed

--- app.js
+++ app.js
@@ -11,12 +11,29 @@ app.use(express.static("public"));
 
 app.get("/" , async (req,res)=> {
   try {
-    const response = await axios.get("https://api.coingecko.com/api/v3/global") ;
-    const result = response.data ;
-    res.render("index.ejs" , {data : result.data}) ;
+    const [globalRes , coinsRes , trendingRes , fngRes , btcRes] = await Promise.all ([
+      axios.get("https://api.coingecko.com/api/v3/global") ,
+      axios.get("https://api.coingecko.com/api/v3/coins/markets", {
+        params: {
+          vs_currency: "usd",
+          order: "market_cap_desc",
+          per_page: 10,
+          page: 1,
+        },
+      }),
+      axios.get("https://api.coingecko.com/api/v3/search/trending"),
+      axios.get("https://api.alternative.me/fng/"),
+      axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin")
+    ]) ;
+    const global = globalRes.data.data ;
+    const coin = coinsRes.data ;
+    const trend = trendingRes.data ;
+    const fng = fngRes.data.data[0] ;
+    const btcData = btcRes.data[0]
+    res.render("index.ejs" , {global , coin , trend , fng , btcData}) ;
   }  catch(error) {
     console.log("Error")
-    res.render("index.ejs" , {data : null})
+    res.render("index.ejs" , {global : null , coin : [] , trend : [] , fng : [] , btcData : []})
   }
 });
 
